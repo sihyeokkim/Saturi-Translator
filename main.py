2c1b35ca08c9c7041db96434668891e6ad4e5231
root <seuyon0101@gmail.com>
2023-03-31 03:52:51 +0000

    update

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
+import os
 import sys
+
+
 sys.path.insert(0,os.getenv('HOME') + '/aiffel/saturi/MODEL/') # vanilla transformer 파일경로에서 불러오기 위해 설정
 sys.path.insert(0,os.getenv('HOME') + '/aiffel/saturi/PRE/')
 sys.path.insert(0,os.getenv('HOME') + '/aiffel/saturi/POST/')
@@ -7,11 +10,19 @@ from translate import translator
 
 if __name__ == '__main__' :
     
-    translator = translator()
+    token_type = input('select - cmsp, msp, spm : \n')
+    vocab_size = input('small_vocab? Yes or No : \n')
+    if (vocab_size == 'yes') | (vocab_size == 'Yes') | (vocab_size == 'y') | (vocab_size == 'Y') :
+        vocab_size = True
+    translator = translator(token_type, vocab_size)
 
     while True :
         print('Add region tag before your input text :')
-        print('jeju : <jj> , kyengsang : <gs> , chunchung : <cc> , kangwon : <kw> , jeonra : <jd> ')
+        print('--------------------------------------------------------------------')
+        print('jeju : <jj> , kyengsang : <gs> , chunchung : <cc> , kangwon : <kw> , jeonra : <jd>')
+        print('--------------------------------------------------------------------')
         input_txt = input()
+        print('**********************************************************************')
         print('input_text : ', input_txt)
         print('translated_text : ',translator.translate(input_txt))
+        print('**********************************************************************\n\n')
--- translate.py
+++ translate.py
@@ -17,35 +17,41 @@ from evaluation import evaluate
 import sentencepiece as spm
 from vanilla_transformer import Transformer, generate_masks
 data_path = os.getcwd() + '/notebook/Preprocessing/'
-
+weight_dir = os.getenv("HOME") + '/aiffel/aiffelthon/final_organizing_folder/transformer_cmsp_16009'
 class trans_former_config :
-    def __init__(self,vocab_small=True) :
+    def __init__(self) :
         self.n_layers =6
         self.d_model=512
         self.n_heads=8
         self.d_ff=2048
-        self.src_vocab_size=16009 if vocab_small else 8009
-        self.tgt_vocab_size=16009 if vocab_small else 8009
+        self.src_vocab_size= 16009
+        self.tgt_vocab_size= 16009
         self.pos_len= 512
         self.dropout=0.2
         self.shared=True
         
-class tokenizer_config(trans_former_config) :
-    def __init__(self, token_type = 'cmsp') :
-        super().__init__()
+class tokenizer_config :
+    def __init__(self, token_type = 'cmsp', vocab_small = False) :
         self.token_type = token_type
-        self.token_vocab_size = 16000 if self.src_vocab_size == 16009 else 8009
+        self.token_vocab_size = 8009 if vocab_small else 16000
         self.enc_token_load_model = data_path + f'spm_enc_spm{self.token_vocab_size}.model'
         self.dec_token_load_model = data_path + f'spm_dec_{self.token_type}{self.token_vocab_size}.model'
-
-class translator(tokenizer_config) :
-    
-    def __init__(self) :
-        super().__init__()
         self.enc_tokenizer = spm.SentencePieceProcessor()
         self.enc_tokenizer.Load(self.enc_token_load_model)
         self.dec_tokenizer = spm.SentencePieceProcessor()
         self.dec_tokenizer.Load(self.dec_token_load_model)
+        
+class translator(trans_former_config) :
+    
+    def __init__(self, token_type = 'cmsp', vocab_small=None) :
+        super().__init__()
+        self.token_type = token_type
+        if vocab_small :
+            self.src_vocab_size= 8009
+            self.tgt_vocab_size= 8009
+            self.tokenizer = tokenizer_config(self.token_type, vocab_small)
+        else :
+            self.tokenizer = tokenizer_config(self.token_type)
         self.model = Transformer(self.n_layers,
                                  self.d_model,
                                  self.n_heads,
@@ -55,8 +61,9 @@ class translator(tokenizer_config) :
                                  self.pos_len,
                                  self.dropout,
                                  self.shared)
-        self.model.load_weights()
+        self.weight_dir = os.getenv("HOME") + f'/aiffel/aiffelthon/final_organizing_folder/transformer_{self.token_type}_{self.src_vocab_size}'
+        self.model.load_weights(self.weight_dir)
         
     def translate(self, input_txt) :
-        ids, result, enc_attns, dec_attns, dec_enc_attns = evaluate(input_txt, self.model, self.enc_tokenizer, self.dec_tokenizer)
+        ids, result, enc_attns, dec_attns, dec_enc_attns = evaluate(input_txt, self.model, self.tokenizer.enc_tokenizer, self.tokenizer.dec_tokenizer)
         return result
